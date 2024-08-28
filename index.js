f0c09c6e3a1dddfdf890625a4a7c6d125969fd6b
DAVID TB <56015064+tbdavid2019@users.noreply.github.com>
2024-08-28 17:44:40 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,74 @@
+require('dotenv').config()
+
+const express = require('express')
+const line = require('@line/bot-sdk')
+const { Configuration, OpenAIApi } = require('openai')
+
+// 初始化 OpenAI 客戶端
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_LINE_SECRET,
+});
+const openai = new OpenAIApi(configuration);
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+}
+
+// create LINE SDK client
+const client = new line.Client(config)
+
+// create Express app
+const app = express()
+
+// register a webhook handler with middleware
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err)
+      res.status(500).end()
+    })
+})
+
+// event handler
+async function handleEvent(event) {
+  try {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null)
+    }
+
+    const userInput = event.message.text.trim()
+    const messages = [
+      {
+        role: 'system',
+        content: 'You are a helpful assistant.',
+      },
+      {
+        role: 'user',
+        content: userInput,
+      },
+    ]
+    
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-4-o',
+      temperature: 1,
+      messages: messages,
+      max_tokens: 1000,
+    })
+
+    const echo = { type: 'text', text: completion.data.choices[0].message.content || '抱歉，我沒有話可說了。' }
+  
+    return client.replyMessage(event.replyToken, [echo])
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// listen on port
+const port = process.env.PORT || 8111
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
