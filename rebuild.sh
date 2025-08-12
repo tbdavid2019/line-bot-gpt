03744e2558349a8296160e934d2f8a171b9fb606
tbdavid2019 <tbdavid2019@gmail.com>
2025-08-12 16:25:17 +0800

    啟動策略

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,16 @@
 sudo docker build -t line-bot-gpt .
-sudo docker run -dp 8111:8111 --env-file .env line-bot-gpt
 
+sudo docker run -dp 8111:8111 \
+  --env-file .env \
+  --name line-bot-gpt \
+  --restart unless-stopped \
+  line-bot-gpt
+
+
+# 停止並刪除舊的容器
+docker stop line-bot-gpt  && docker rm line-bot-gpt
+# 刪除無用的舊 image
+docker image prune -f
+
+
+pm2 start index2.js --name "my-line-bot"
